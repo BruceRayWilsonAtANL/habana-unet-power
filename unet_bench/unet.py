3bf11dd534dc3207f5f7a6c42a52689f8ae2e0d6
Andre Evard <andre@Andre-Mac.local>
2022-08-15 15:39:49 -0400

    Swapped back eval condition

--- unet_bench/unet.py
+++ unet_bench/unet.py
@@ -244,14 +244,16 @@ def eval(args, model, loss_fn, testloader, device, epoch):
         )
 
     save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"performance/{args.run_name}.txt")
-    mean_dsc = np.mean(
-        dsc_per_volume(
-            validation_pred,
-            validation_true,
-            testloader.dataset.patient_slice_index,
-        )
-    )
+    
     print(log_loss_summary(loss_valid, epoch, prefix="val_"))
+    if args.rank == 0:
+        mean_dsc = np.mean(
+            dsc_per_volume(
+                validation_pred,
+                validation_true,
+                testloader.dataset.patient_slice_index,
+            )
+        )
     if not args.distributed:
         log(save_path, f"eval,{epoch},{time.time() - eval_time},{np.mean(loss_valid)}, {mean_dsc}")
         print(log_scalar_summary("val_dsc", mean_dsc, epoch))
@@ -327,14 +329,13 @@ def main():
         train(args, model, loss_fn, optimizer, loader_train, device, epoch)
         total_train_time += (time.time() - st_timer)
 
-        if args.rank == 0:
-            st_timer = time.time()
-            dsc = eval(args, model, loss_fn, loader_valid, device, epoch)
-            total_eval_time += (time.time() - st_timer)
+        st_timer = time.time()
+        dsc = eval(args, model, loss_fn, loader_valid, device, epoch)
+        total_eval_time += (time.time() - st_timer)
 
-            if dsc > best_validation_dsc:
-                best_validation_dsc = dsc
-                torch.save(model.state_dict(), os.path.join(dir_path, f"weights/{args.weights_file}"))
+        if dsc > best_validation_dsc:
+            best_validation_dsc = dsc
+            torch.save(model.state_dict(), os.path.join(dir_path, f"weights/{args.weights_file}"))
 
     # Log all the info not already logged in train/eval
     if not args.distributed:
