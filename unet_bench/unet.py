16ed8dfc38059ddecd4e46f765c7c837cefd34bf
Andre Evard <andre@Andre-Mac.local>
2022-08-15 15:53:17 -0400

    Fixed dsc warning

--- unet_bench/unet.py
+++ unet_bench/unet.py
@@ -175,8 +175,10 @@ def dsc_per_volume(validation_pred, validation_true, patient_slice_index):
     for p in range(len(num_slices)):
         y_pred = np.array(validation_pred[index : index + num_slices[p]])
         y_true = np.array(validation_true[index : index + num_slices[p]])
-        dsc_list.append(dsc(y_pred, y_true))
         index += num_slices[p]
+        if len(y_true) == 0 or len(y_pred) == 0:
+            continue
+        dsc_list.append(dsc(y_pred, y_true))
     return dsc_list
 
 def train(args, model, loss_fn, optimizer, loader_train, device, epoch):
@@ -246,20 +248,18 @@ def eval(args, model, loss_fn, testloader, device, epoch):
     save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"performance/{args.run_name}.txt")
     
     print(log_loss_summary(loss_valid, epoch, prefix="val_"))
-    if args.rank == 0:
-        mean_dsc = np.mean(
-            dsc_per_volume(
-                validation_pred,
-                validation_true,
-                testloader.dataset.patient_slice_index,
-            )
+    mean_dsc = np.mean(
+        dsc_per_volume(
+            validation_pred,
+            validation_true,
+            testloader.dataset.patient_slice_index,
         )
+    )
     if not args.distributed:
         log(save_path, f"eval,{epoch},{time.time() - eval_time},{np.mean(loss_valid)}, {mean_dsc}")
         print(log_scalar_summary("val_dsc", mean_dsc, epoch))
     elif args.distributed and args.rank == 0:
         log(save_path, f"eval,{args.rank},{epoch},{time.time() - eval_time},{np.mean(loss_valid)}, nan")
-    # log(save_path, log_scalar_summary("val_dsc", mean_dsc, epoch))
     return mean_dsc
 
 def log(save_path, line):
